3a934fbcc53b9645d77b49fb4841faa415005bb1
Tarik oz <a.tarik2005@gmail.com>
2022-09-14 11:33:06 +0300

    navbar bitti

--- app.js
+++ app.js
@@ -8,7 +8,22 @@ if(toggle&&nav){
 
 }
 
+}
+showMenu('nav-toggle', 'nav-menu');
+
+
+const navLink = document.querySelectorAll(".nav__link");
+
+function linkAction(){
+const navMenu = document.getElementById("nav-menu");
+navMenu.classList.remove("show-menu");
 
 
 }
-showMenu('nav-toggle', 'nav-menu');
+
+navLink.forEach((n) => n.addEventListener("click",linkAction) );
+
+//? **************navbar bitti***********
+
+
+
